8e5cfd7956450fcc3bc924de146981112bfa816f
eunsu28 <ayay.e2008@gmail.com>
2021-07-23 18:38:17 +0800

    facebook completed
    홀롤롤로

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ import urllib.request
 
 URL = "https://www.instagram.com/dlwlrma/"
 
-driver = webdriver.Chrome("chromedriver")
+driver = webdriver.Chrome("/Users/kim-eunsu/Desktop/click/chromedriver")
 driver.get(URL)
+
+sleep(5)
+
+facebook_login_btn = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[5]/button')
+sleep(5) 
+facebook_login_btn.click()
+
